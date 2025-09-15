a0630ea3eef5a60589d3fb1a424916c66161ddd5
kovokar <pkovokar@gmail.com>
2025-09-15 01:14:00 -0300

    feat(DB): db config

---
+++ internal/database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func StartDb() {
+	strConn := "host=localhost user=admin password=123456 dbname=books port=25432 sslmode=disable"
+
+	connection, err := gorm.Open(postgres.Open(strConn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect to database: ", err)
+	}
+
+	db = connection
+	config, _ := db.DB()
+
+	config.SetConnMaxLifetime(10)
+	config.SetMaxOpenConns(100)
+	config.SetMaxIdleConns(10)
+}
+
+func GetDatabase() *gorm.DB {
+	return db
+}
