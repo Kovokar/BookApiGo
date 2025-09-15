43b245806934cea8381174203c69bda333cb4de8
kovokar <pkovokar@gmail.com>
2025-09-15 20:29:37 -0300

    feat: implement middleware

---
+++ internal/middleware/auth_middleware.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+	"github.com/Kovokar/BookApiGo/internal/services"
+	"github.com/gin-gonic/gin"
+)
+
+func Auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		const Bearer_schema = "Bearer "
+		header := c.GetHeader("Authorization")
+		if header == "" {
+			c.AbortWithStatus(401)
+		}
+
+		token := header[len(Bearer_schema):]
+
+		if !services.NewJWTService().ValidateToken(token) {
+			c.AbortWithStatus(401)
+		}
+	}
+}
--- internal/routes/books.go
+++ internal/routes/books.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"github.com/Kovokar/BookApiGo/internal/controllers"
+	"github.com/Kovokar/BookApiGo/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 // Função que registra as rotas relacionadas a "books"
 func BooksRoutes(router *gin.RouterGroup) {
-	books := router.Group("/books")
+	books := router.Group("/books", middleware.Auth())
 	{
 		books.GET("/", controllers.GetBook)
 		books.GET("/:id", controllers.GetBookById)
